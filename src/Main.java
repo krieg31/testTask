7ef1cebaf33d3911d1c5676c3874f7765d6e7cbd
Aleksei <alexyburtsev@gmail.com>
2023-08-25 18:14:23 +0900

    1st commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-import static java.lang.Math.abs;
+import com.google.gson.*;
 
-class Client
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+class Passenger
 {
+    private static final DecimalFormat df = new DecimalFormat("0.000");
     public String name;
     public int id;
-    public int cooridinateA1;
-    public int cooridinateA2;
-    public int cooridinateB1;
-    public int cooridinateB2;
-
-    public Client(String name, int id, int cooridinateA1, int cooridinateA2, int cooridinateB1, int cooridinateB2)
+    public double cooridinateA1, cooridinateA2, cooridinateB1, cooridinateB2;
+    public Passenger(String name, int id, double cooridinateA1, double cooridinateA2, double cooridinateB1, double cooridinateB2)
     {
         this.name = name;
         this.id = id;
@@ -18,18 +23,18 @@ class Client
         this.cooridinateB1 = cooridinateB1;
         this.cooridinateB2 = cooridinateB2;
     }
-    public static Client CreateClient(int ClientId)
+    public static Passenger CreateClient(int ClientId)
     {
         String name = "Client"+ClientId;
         int id = ClientId;
-        int coordinateA1 = getRandomNumber();
-        int cooridinateA2 = getRandomNumber();
-        int cooridinateB1 = getRandomNumber();
-        int cooridinateB2 = getRandomNumber();
+        double coordinateA1 = getRandomEcoordinate();
+        double cooridinateA2 = getRandomNcoordinate();
+        double cooridinateB1 = getRandomEcoordinate();
+        double cooridinateB2 = getRandomNcoordinate();
 
-        return new Client(name , id , coordinateA1, cooridinateA2, cooridinateB1, cooridinateB2);
+        return new Passenger(name , id , coordinateA1, cooridinateA2, cooridinateB1, cooridinateB2);
     }
-    public Client PrintClient()
+    public Passenger PrintClient()
     {
         System.out.println("name = " + this.name);
         System.out.println("id = " + this.id);
@@ -40,85 +45,65 @@ class Client
 
         return null;
     }
-    public static int getRandomNumber() {
-        int min = 0;
-        int max = 100;
-        return (int) ((Math.random() * (max - min)) + min);
+    public static double getRandomNcoordinate() {
+        double min = 62.028;
+        double max = 62.038;
+        double coordinate = ((Math.random() * (max - min)) + min);
+        BigDecimal rounded = new BigDecimal(coordinate);
+        BigDecimal rounded1 = rounded.setScale(7, RoundingMode.HALF_UP);
+        return rounded1.doubleValue();
+    }
+    public static double getRandomEcoordinate() {
+        double min = 129.727;
+        double max = 129.762;
+        double coordinate = ((Math.random() * (max - min)) + min);
+        BigDecimal rounded = new BigDecimal(coordinate);
+        BigDecimal rounded1 = rounded.setScale(7, RoundingMode.HALF_UP);
+        return rounded1.doubleValue();
     }
 }
 
 
 public class Main {
-    public static int CalculateDistance(int cooridinateA1, int cooridinateA2,
-                                        int cooridinateA3, int cooridinateA4,
-                                        int cooridinateB1, int cooridinateB2,
-                                        int cooridinateB3, int cooridinateB4)
-    {
-        //так-как в нашем случае нельзя "ходить" по диагонали,
-        //количество ходов для перехода от одной ячейки к другой вычисляется очень просто
-        int x,y,x1,y1, x0, y0;
-
-        // x+y - количество шагов от места старта водителя до местонахождения клиента
-        x = abs(cooridinateA1-cooridinateA3);
-        y = abs(cooridinateA2-cooridinateA4);
-
-        // x0+y0 - количество шагов от местонахождения клиента до его точки назначения
-        x0 = abs(cooridinateA1-cooridinateB1);
-        y0 = abs(cooridinateA2-cooridinateB2);
-
-        // x1+y1 - количество шагов от высадки пассажира до конечной точки водителя
-        x1 = abs(cooridinateB1-cooridinateB3);
-        y1 = abs(cooridinateB2-cooridinateB4);
-
-        // суммируем общее количество шагов для прохождения маршрута
-        return(x+y+x1+y1+x0+y0);
-    }
-
     public static void main(String[] args) {
 
         //создаю "Водителя", использую класс клиента, т.к. создавать
         //отдельный класс в данном случае излишне, у него id = 0
 
-        Client Driver = Client.CreateClient(0);
+        Passenger Driver = Passenger.CreateClient(0);
 
         //создаю массив из 5 клиентов, id начинаются с 1
-        Client[] ArrayOfClients = new Client[5];
-        for (int i = 1; i < ArrayOfClients.length; i++)
+        Passenger[] arrayOfPassengers = new Passenger[5];
+        for (int i = 1; i < arrayOfPassengers.length; i++)
         {
-            ArrayOfClients[i] = Client.CreateClient(i);
+            arrayOfPassengers[i] = Passenger.CreateClient(i);
         }
-        //создаю контейнер для хранения количества шагов; 200 - максимальное число для "карты" 100х100
-        int optimalDistance=200;
 
-        //создаю конейнер для хранения id клиента, с ближайшим маршрутом
-        //значение по-умолчанию если клиент всего один
-        int optimalClientId = 1;
+        //Использую бесплатное API openrouteservice
+
+        /**/
+        Client client = ClientBuilder.newClient();
+        Entity<String> payload = Entity.json("{\"locations\":[[9.70093,48.477473],[9.207916,49.153868],[37.573242,55.801281],[115.663757,38.106467]]," +
+                "\"metrics\":[\"duration\",\"distance\"]}");
+        Response response = client.target("https://api.openrouteservice.org/v2/matrix/driving-car")
+                .request()
+                .header("Authorization", "5b3ce3597851110001cf6248789dcd5238624851bb77937a910c3660")
+                .header("Accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8")
+                .header("Content-Type", "application/json; charset=utf-8")
+                .post(payload);
+
+        System.out.println("status: " + response.getStatus());
+        System.out.println("headers: " + response.getHeaders());
+        System.out.println("body:" + prettyPrintUsingGson(response.readEntity(String.class)));
+
+    }
 
 
-        for (int i = 1; i < ArrayOfClients.length; i++)
-        {
-            int temp = CalculateDistance(
-                    ArrayOfClients[i].cooridinateA1, ArrayOfClients[i].cooridinateA2,
-                    Driver.cooridinateA1, Driver.cooridinateA2,
-                    ArrayOfClients[i].cooridinateB1, ArrayOfClients[i].cooridinateB2,
-                    Driver.cooridinateB1, Driver.cooridinateB2);
-
-            if (optimalDistance>= temp)
-            {
-                optimalDistance = temp;
-                optimalClientId = ArrayOfClients[i].id;
-            }
-        }
-        System.out.println("Водитель : ");
-        Driver.PrintClient();
-            System.out.println();
-            System.out.println();
-
-        System.out.println("Клиент с попутным маршрутом : ");
-        ArrayOfClients[optimalClientId].PrintClient();
-            System.out.println();
-            System.out.println();
-
-        System.out.println("Суммарная дистанция = " + optimalDistance);
+    //Это для чтобы вывод JSONa был приятнее
+    public static String prettyPrintUsingGson(String uglyJson) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonElement jsonElement = JsonParser.parseString(uglyJson);
+        String prettyJsonString = gson.toJson(jsonElement);
+        return prettyJsonString;
     }
 }
